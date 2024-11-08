98a5bfbefac2cf25ac687810d8d7b2fee461fb58
Thammahetti Thejan <thammahetti.m.t@iisgalvanimi.edu.it>
2024-11-08 12:02:14 +0000

    Commit 1 risolto

--- data_adder.py
+++ data_adder.py
@@ -1,46 +1,35 @@
 import mysql.connector
 import json
 
-
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="verifica"  #
+    database="verifica"
 )
 
-
 def carica_dati_json():
+    with open('citta.json', 'r') as f:
+        dati = json.load(f)
 
-        with open('citta.json', 'r') as f:
-            dati = json.load(f)
+    mycursor = mydb.cursor()
 
-     
-        mycursor = mydb.cursor()
+    for record in dati:
+        città = record['Città']
+        paese = record['Paese']
+        popolazione = record['Popolazione']
+        monumenti = record['Monumenti principali']
 
-   
-        for record in dati:
-            ID = record['ID']
-            città = record['Città']
-            paese = record['Paese']
-            popolazione = record['Popolazione']
-            monumenti = record['Monumenti principali']
+        # Correzione della query
+        mycursor.execute("""
+            INSERT INTO citta (Città, Paese, Popolazione, Monumenti_principali)
+            VALUES (%s, %s, %s, %s)
+        """, (città, paese, popolazione, monumenti))
 
-          
-            mycursor.execute("""
-                INSERT INTO citta (ID, Città, Paese, Popolazione, Monumenti_principali)
-                VALUES (%s, %s, %s, %s, %s)
-            """, (ID, città, paese, popolazione, monumenti))
-
-      
-        mydb.commit()
-        print("Dati caricati con successo!")
-        
- 
-        mycursor.close()
+    mydb.commit()
+    print("Dati caricati con successo!")
     
- 
-
+    mycursor.close()
 
 if __name__ == '__main__':
     carica_dati_json()
